3075a2d440971f66d97e4c566a8827748b08e142
Dan White <danw@pipeworks.com>
2015-01-13 22:03:58 -0800

    Cleanup and refactor.

--- GFLib.h
+++ GFLib.h
@@ -14,6 +14,7 @@
 using namespace std;
 
 using uint = unsigned _int64;
+using int64 = long long;
 
 // Utilities
 int HighBit(uint val);
@@ -21,3 +22,35 @@ int CountBits(uint val);
 string UintToStr(uint val);
 uint StrToUint(const char *pc);
 void TestConvert();
+
+int64 EuclideanGCD(int64 a, int64 b);
+
+struct GCDRes
+{
+	int64 afactor;
+	int64 bfactor;
+	int64 gcd;
+
+	bool operator ==(const GCDRes &other)
+	{
+		return afactor == other.afactor && bfactor == other.bfactor && gcd == other.gcd;
+	}
+};
+
+// Given a, b, find the gcd(a,b) and numbers so that afactor * a + bfactor * b = gcd
+GCDRes ExtendedEucliedean(int64 a, int64 b);
+
+// Given an element a cyclic group, and the prime factorization of the order of the group,
+// find the decompostion into the director product of cyclic groups of prime power order.
+void AbelienianDecomp(const int64 val, int64 ndim, const int64 *sizes, int64 *pres);
+
+// Given elements of groups of prime power order, find the value in the corresponding 
+// cyclic group.
+int64 AbelienianReverse(int64 ndim, const int64 *powers, const int64 *mods);
+
+
+void TestGFUtils()
+#ifdef NDEBUG
+{}
+#endif
+;
--- GFUtils.cpp
+++ GFUtils.cpp
@@ -1,7 +1,8 @@
 
 #include "GFLib.h"
-
-
+#include <memory>
+#include <utility>
+#include <vector>
 ///////////////////////////////////////////////////////////////////////////////
 int HighBit(uint val)
 {
@@ -60,6 +61,102 @@ uint StrToUint(const char *pc)
 	return res;
 }
 ///////////////////////////////////////////////////////////////////////////////
+int64 EuclideanGCD(int64 a, int64 b)
+{
+	// Note that this works even if b > a, as the first iteration will just end
+	// up swapping the two.
+	while (b != 0)
+	{
+		auto t = a % b;
+		a = b;
+		b = t;
+	}
+	return a;
+}
+
+GCDRes ExtendedEucliedean(int64 a, int64 b)
+{
+	struct PartialEE { int64 s, t, r; };
+
+	PartialEE prev, cur, next;
+
+	prev.s = 1;
+	prev.t = 0;
+	prev.r = a;
+
+	cur.s = 0;
+	cur.t = 1;
+	cur.r = b;
+
+	while (cur.r != 0)
+	{
+		auto q = prev.r / cur.r;
+
+		next.s = prev.s - cur.s * q;
+		next.t = prev.t - cur.t * q;
+		next.r = prev.r - cur.r * q;
+
+		prev = cur;
+		cur = next;
+	}
+
+	GCDRes res;
+	res.afactor = prev.s;
+	res.bfactor = prev.t;
+	res.gcd = prev.r;
+
+	assert(res.afactor * a + res.bfactor * b == res.gcd);
+	return res;
+}
+///////////////////////////////////////////////////////////////////////////////
+void AbelienianDecomp(const int64 val, int64 ndim, const int64 *sizes, int64 *pres)
+{
+	for (int64 i = 0; i < ndim; i++)
+		pres[i] = val % sizes[i];
+
+	return;
+}
+///////////////////////////////////////////////////////////////////////////////
+int64 _AbelienianReverse(int64 s1, int64 s2, int64 m1, int64 m2)
+{
+
+	int64 del = m2 - m1;
+
+	auto r = ExtendedEucliedean(s1, s2);
+	assert(r.gcd == 1);
+
+	int64 res;
+	res = r.afactor * s1 * del + m1;
+
+	// We want the result module p. Unfortunately, the CRT rounds towards
+	// 0, not down, so we have to offset so that we will have a positive value.
+	auto p = s1 * s2;
+	res += p * p;
+	assert(res >= 0);
+	res %= p;
+
+	assert(res >= 0 && res < s1 * s2);
+	assert(res % s1 == m1);
+	assert(res % s2 == m2);
+
+	return res;
+}
+///////////////////////////////////////////////////////////////////////////////
+int64 AbelienianReverse(int64 ndim, const int64 *powers, const int64 *mods)
+{
+	assert(ndim >= 2);
+	int64 cp = powers[0];
+	int64 cm = mods[0];
+
+	for (int64 i = 1; i < ndim; i++)
+	{
+		cm = _AbelienianReverse(cp, powers[i], cm, mods[i]);
+		cp *= powers[i];
+	}
+	return cm;
+}
+///////////////////////////////////////////////////////////////////////////////
+#ifndef NDEBUG
 void TestConvert()
 {
 	const char *ps1 = "0010010";
@@ -72,3 +169,57 @@ void TestConvert()
 	auto s1 = UintToStr(v1);
 	assert(s1.compare(ps1) == 0);
 }
+///////////////////////////////////////////////////////////////////////////////
+void TestDecomp(const int64 *powers)
+{
+	int64 prod = 1;
+	int64 np;
+	for (np = 0; powers[np]; np++)
+	{
+		prod *= powers[np];
+	}
+	assert(prod < 10000000000);	// Amything larger will take a very long time.
+
+	vector<int64> mods;
+	mods.resize((unsigned int)prod);
+	for (int64 i = 0; i < prod; i++)
+	{
+		AbelienianDecomp(i, np, powers, mods.data());
+		int64 rev = AbelienianReverse(np, powers, mods.data());
+		assert(i == rev);
+	}
+}
+///////////////////////////////////////////////////////////////////////////////
+void TestGCDExp()
+{
+	GCDRes ap;
+
+	ap = ExtendedEucliedean(27 * 2 * 3, 83 * 83 * 3);
+	ap = ExtendedEucliedean(-27 * 2 * 3, 83 * 83 * 3);
+	ap = ExtendedEucliedean(-27 * 2 * 3, -83 * 83 * 3);
+	ap = ExtendedEucliedean(27 * 2 * 3, -83 * 83 * 3);
+
+	int64 r, r1;
+	r = EuclideanGCD(83 * 83 * 3, 27 * 2 * 3);
+	r1 = EuclideanGCD(27 * 2 * 3, 83 * 83 * 3);
+	assert(r == r1);
+
+	int64 pi[] = { 8, 9, 5, 0 };
+	TestDecomp(pi);
+
+	int64 pi1[] = { 4, 83 * 83, 9, 5, 0 };
+	TestDecomp(pi1);
+
+
+	//cout << _AbelienianReverse(4, 5, 3, 4);
+
+}
+///////////////////////////////////////////////////////////////////////////////
+
+void TestGFUtils()
+{
+	TestConvert();
+	TestGCDExp();
+
+}
+#endif
